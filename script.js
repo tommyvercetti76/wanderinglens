eba41be8713dd9d981f5b27449942fc5115f7fde
tommyvercetti76 <tommyvercetti76@github.com>
2023-07-08 13:07:46 -0500

    Added environment and gitignores

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const images = [
-        { src: './images/Cascades.PNG', alt: 'A Crow at Cascades' },
-        { src: './images/Sunset_Clouds_Needle.PNG', alt: 'Space Needle at Sunset' },
-        { src: './images/Sunset_Clouds.PNG', alt: 'Raining Color' },
-    ];
+// Import Firebase
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-    const imageGrid = document.getElementById('image-grid');
-    const imageOverlay = document.getElementById('image-overlay');
-    const overlayImage = document.getElementById('overlay-image');
-    const closeBtn = document.getElementById('close-btn');
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+};
 
-    images.forEach(image => {
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const imageGrid = document.getElementById('image-grid');
+const imageOverlay = document.getElementById('image-overlay');
+const overlayImage = document.getElementById('overlay-image');
+const closeBtn = document.getElementById('close-btn');
+
+// Get a reference to the Firestore database
+const db = firebase.firestore();
+
+// Query the collection "images"
+db.collection('images').get().then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+        const image = doc.data();
+        
         const imageItem = document.createElement('div');
         imageItem.className = 'image-item';
 
@@ -27,8 +44,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
             imageOverlay.classList.remove('hidden');
         };
     });
-
-    closeBtn.onclick = () => {
-        imageOverlay.classList.add('hidden');
-    };
 });
+
+closeBtn.onclick = () => {
+    imageOverlay.classList.add('hidden');
+};
